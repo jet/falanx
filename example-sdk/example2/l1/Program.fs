1224ee8f4235e7bcf69baa7b7c0b29643e7b0dbc
Enrico Sada <enrico.sada@jet.com>
2018-10-03 15:00:45 +0200

    add deserialization example

--- example-sdk/example2/l1/Program.fs
+++ example-sdk/example2/l1/Program.fs
@@ -15,7 +15,14 @@ module Program =
         let buffer = new Froto.Serialization.ZeroCopyBuffer(1000);
         BundleRequest.Serialize(r, buffer)
 
+        printfn "Serialized:"
         printfn "%A" (buffer.Array)
+
+        let buffer2 = new Froto.Serialization.ZeroCopyBuffer(buffer.Array);
+        let s = BundleRequest.Deserialize(buffer2)
+
+        printfn "Deserialized:"
+        printfn "%A" s
 
         0
 
