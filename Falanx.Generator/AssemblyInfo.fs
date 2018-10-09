4171fbb1420b7fec6e60a70dd4ae91f42bbf7f7e
Enrico Sada <enrico@sada.io>
2018-10-09 13:38:49 +0200

    add Falanx.Tool as .NET Tool and bundle it in Falanx.Sdk (#29)
    fix #24 
    
    - remove `dotnet-Falanx.Generator` cli tool in `Falanx.Generator` package
    - add `Falanx.Tool` as .NET tool ( can be installed with `dotnet tool install` )
    - bundle `Falanx.Tool` inside `Falanx.Sdk`, so just that package is needed
    - do not publish anymore `Falanx.BinaryGenerator` nupkg, it's now an internal assembly

--- Falanx.Generator/AssemblyInfo.fs
+++
@@ -1,7 +0,0 @@
-namespace Falanx.Generator
-open System.Reflection
-
-[<assembly : AssemblyCompany "Jet.com">]
-[<assembly : AssemblyVersion "0.1.0">]
-
-do ()
---
+++ Falanx.Tool/Program.fs
@@ -0,0 +1,43 @@
+﻿// Learn more about F# at http://fsharp.org
+namespace Falanx.Generator
+open System
+open System.IO
+open Argu
+open Falanx.BinaryGenerator
+open Falanx.JsonCodec
+
+module main =
+
+    type Arguments =
+        | [<Mandatory>] InputFile of string
+        | [<Mandatory>] DefaultNamespace of string
+        | [<Mandatory>] OutputFile of string
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | InputFile _ -> "specify a proto file to input."
+                | DefaultNamespace _ -> "specify a default namespace to use for code generation."
+                | OutputFile _ -> "Specify the file name that the generated code will be written to."
+    
+    
+    let parser = ArgumentParser.Create<Arguments>(programName = "dotnet-Falanx.Generator")
+    
+    [<EntryPoint>]
+    let main argv =
+        try
+            let code = temp.tryCode()
+                        
+            let results = parser.Parse argv
+            let inputFile = results.GetResult InputFile
+            let outputFile = results.GetResult OutputFile
+            let defaultNamespace = results.GetResult DefaultNamespace
+            let protoDef = IO.File.ReadAllText inputFile
+            printfn "Generating code for: %s" inputFile
+            Proto.createFSharpDefinitions(protoDef, outputFile, defaultNamespace)
+        with
+        | :? FileNotFoundException as fnf ->
+            printfn "ERROR: inputfile %s doesn not exist\n%s" fnf.FileName (parser.PrintUsage())
+        | ex -> printfn "%s\n%s" (parser.PrintUsage()) ex.Message
+    
+        0 // return an integer exit code
