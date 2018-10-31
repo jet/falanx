a11fb93f2113cc01377aa21297538350607258ee
Enrico Sada <enrico.sada@jet.com>
2018-10-31 18:59:06 +0100

    to file

--- test/examples/sample7 scala/app/src/main/scala/Program.scala
+++ test/examples/sample7 scala/app/src/main/scala/Program.scala
@@ -1,13 +1,35 @@
-object Program extends App {
-  println("Hello, World!")
+import java.nio.file.{Files, Paths}
 
-  val example = new BinaryExample
+object Program {
 
-  val bytes = example.serialize()
-  println(bytes)
+  sealed trait Command
+  case class Serialize(path: String) extends Command
+  case class Deserialize(path: String) extends Command
 
-  var item2 = example.deserialize(bytes)
 
-  println(item2)
+  def main(args: Array[String]): Unit = {
 
+    val cmd =
+      args match {
+        case Array("--serialize", p) => Serialize(p)
+        case Array("--deserialize", p) => Deserialize(p)
+        case _ => throw new Exception("invalid args, expecting --serialize or --deserialize")
+      }
+
+    val example = new BinaryExample
+
+    cmd match {
+      case Serialize(path) => {
+        val bytes = example.serialize()
+        println(bytes)
+        Files.write(Paths.get(path), bytes)
+      }
+      case Deserialize(path) => {
+        val bytes = Files.readAllBytes(Paths.get(path))
+        val item2 = example.deserialize(bytes)
+        println(item2)
+      }
+    }
+
+  }
 }
