ab6b75799af116e88ff2cad8f99352417298d46b
Enrico Sada <enrico.sada@jet.com>
2018-10-31 15:01:46 +0100

    add serialize/deserialize

--- test/examples/sample7 scala/app/src/main/scala/Program.scala
+++ test/examples/sample7 scala/app/src/main/scala/Program.scala
@@ -1,3 +1,13 @@
 object Program extends App {
   println("Hello, World!")
+
+  val example = new BinaryExample
+
+  val bytes = example.serialize()
+  println(bytes)
+
+  var item2 = example.deserialize(bytes)
+
+  println(item2)
+
 }
