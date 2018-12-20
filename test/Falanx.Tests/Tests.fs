dcade7ed22a9088fa30ed9f2d93ff96580ca0538
Enrico Sada <enrico.sada@jet.com>
2018-12-20 14:39:30 +0100

    use test assets from protobuff repo

--- test/Falanx.Tests/Tests.fs
+++ test/Falanx.Tests/Tests.fs
@@ -5,15 +5,24 @@ open System.IO
 open Expecto
 open Expecto.Logging
 open Expecto.Logging.Message
+open Fleece
+open Newtonsoft
 
-let generalTests =
-  testList "general" [
-    testCase "simple1" (fun () ->
-      ()
+let jsonTests =
+  testList "json" [
+    testCase "Original field name accepted" (fun () ->
+        let json = """{ "single_int32": 10 }"""
+        match parseJson json with
+            | Ok(msg) -> 
+                Expect.equal (Some(10)) msg.singleInt32 "Failed to deserialize singleInt32"
+                Expect.equal (Some(0L)) msg.singleInt64 "Expected default 0 on singleInt64"
+                Expect.equal (Some("")) msg.singleString "Expected default empty string"
+            | Error(err) -> 
+                failwithf "Failed to parse json: %s" err
     )
   ]
 
 let tests () =
-  [ generalTests ]
+  [ jsonTests ]
   |> testList "unit"
   |> testSequenced
