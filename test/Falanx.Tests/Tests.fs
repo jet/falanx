cb57ad839d00bf837c01983ecca85f7a2c42d726
Enrico Sada <enrico.sada@jet.com>
2018-12-20 15:19:20 +0100

    f

--- test/Falanx.Tests/Tests.fs
+++ test/Falanx.Tests/Tests.fs
@@ -7,18 +7,20 @@ open Expecto.Logging
 open Expecto.Logging.Message
 open Fleece
 open Newtonsoft
+open protobuf_unittest3
 
 let jsonTests =
   testList "json" [
     testCase "Original field name accepted" (fun () ->
         let json = """{ "single_int32": 10 }"""
         match parseJson json with
-            | Ok(msg) -> 
-                Expect.equal (Some(10)) msg.singleInt32 "Failed to deserialize singleInt32"
-                Expect.equal (Some(0L)) msg.singleInt64 "Expected default 0 on singleInt64"
-                Expect.equal (Some("")) msg.singleString "Expected default empty string"
-            | Error(err) -> 
-                failwithf "Failed to parse json: %s" err
+        | Result.Ok msg ->
+            printfn "%A" msg
+            Expect.equal (Some(10)) msg.singleInt32 "Failed to deserialize singleInt32"
+            Expect.equal (Some(0L)) msg.singleInt64 "Expected default 0 on singleInt64"
+            Expect.equal (Some("")) msg.singleString "Expected default empty string"
+        | Result.Error err ->
+            failwithf "Failed to parse json: %A" err
     )
   ]
 
