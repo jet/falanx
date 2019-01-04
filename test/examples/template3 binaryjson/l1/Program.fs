a44a0eb4f0d6bf066a4c55b6e9f2551decce4a66
Enrico Sada <enrico.sada@jet.com>
2019-01-04 12:30:55 +0100

    reenable json test with repeated

--- test/examples/template3 binaryjson/l1/Program.fs
+++ test/examples/template3 binaryjson/l1/Program.fs
@@ -1,29 +1,62 @@
 namespace l1
 
+open System.IO
+
 module Program =
+
+    type Command =
+        | Serialize of string * string
+        | Deserialize of string * string * string
 
     [<EntryPoint>]
     let main args =
 
-        let bytes = BinaryExample.serialize ()
+        let cmd =
+            match args with
+            | [| "--format"; format; "--serialize"; path |] -> Serialize (format, path)
+            | [| "--format"; format; "--deserialize"; path; "--out"; out |] -> Deserialize (format, path, out)
+            | _ -> failwithf "invalid args, expecting --serialize or --deserialize, but was '%A'" args
 
-        printfn "[BINARY] Serialized:"
-        printfn "%A" bytes
+        match cmd with
+        | Serialize ("binary", path) ->
+            let bytes = BinaryExample.serialize ()
 
-        let s = BinaryExample.deserialize bytes
+            printfn "Serialized:"
+            printfn "%A" bytes
 
-        printfn "[BINARY] Deserialized:"
-        printfn "%A" s
+            File.WriteAllBytes(path, bytes)
+            0
+        | Deserialize ("binary", path, outputPath) ->
+            let bytes = File.ReadAllBytes(path)
 
-        let jsonText = JsonExample.serialize ()
+            let s = BinaryExample.deserialize bytes
 
-        printfn "[JSON] Serialized:"
-        printfn "%A" jsonText
+            printfn "Deserialized:"
+            printfn "%A" s
 
-        let s = JsonExample.deserialize jsonText
+            File.WriteAllText(outputPath, sprintf "%A" s, System.Text.Encoding.UTF8)
+            0
+        | Serialize ("json", path) ->
+            let bytes = JsonExample.serialize ()
 
-        printfn "[JSON] Deserialized:"
-        printfn "%A" s
+            printfn "Serialized:"
+            printfn "%A" bytes
 
-        0
+            File.WriteAllText(path, bytes)
+            0
+        | Deserialize ("json", path, outputPath) ->
+            let jsonText = File.ReadAllText(path)
 
+            let s = JsonExample.deserialize jsonText
+
+            printfn "Deserialized:"
+            printfn "%A" s
+
+            File.WriteAllText(outputPath, sprintf "%A" s, System.Text.Encoding.UTF8)
+            0
+        | Serialize (format, _) ->
+            printfn "format %s not supported, avaiaible 'json' or 'binary'" format
+            2
+        | Deserialize (format, _, _) ->
+            printfn "format %s not supported, avaiaible 'json' or 'binary'" format
+            2
